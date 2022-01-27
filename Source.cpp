6729592660ae7462356e362c5eefb0ac740299ee
joel-rodgers <joel.kobara@strathmore.edu>
2022-01-28 01:04:12 +0300

    joel commits

--- Source.cpp
+++ Source.cpp
@@ -25,3 +25,82 @@ struct {
     int x;
     int y;
 } s[100];
+
+class Fruct {
+public:
+    int x, y;
+    void New()
+    {
+        x = rand() % N;
+        y = rand() % (M - 3);
+    }
+    void DrawFruct()
+    {
+        glColor3f(1.0, 0.9, 0.0);
+        glRectf(x * Scale, y * Scale, (x + 1) * Scale, (y + 1) * Scale);
+    }
+} m[2];
+
+class Bomb {
+public:
+    int x, y;
+
+    void New()
+    {
+        x = rand() % N;
+        y = rand() % (M - 3);
+    }
+    void DrawBomb()
+    {
+        glColor3f(1.0, 0.0, 0.0);
+        glBegin(GL_POLYGON);
+        for (float i = 0; i < 2 * 3.14; i += 3.14 / 4) {
+            glVertex2f((x + 0.5) * Scale + (0.5) * Scale * (1.1) * sin(i),
+                (y + 0.5) * Scale + (0.5) * Scale * (1.1) * cos(i));
+        }
+        glEnd();
+    }
+} u[10];
+
+void draw_string(void* font, const char* string)
+{
+    while (*string)
+        glutStrokeCharacter(font, *string++);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void CreateGlutWindow()
+{
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA); // Mode selection: single buffer and RGBA colors
+    glutInitWindowSize(w, h);
+    glutCreateWindow("Snake");
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, w, 0, h);
+}
+
+void timer(int = 0)
+{
+    if (d == 2) {
+        display();
+    }
+    Tick();
+
+    glutTimerFunc(80, timer, 0);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    CreateGlutWindow();
+    glutDisplayFunc(display);
+    glutTimerFunc(80, timer, 0);
+    glutSpecialFunc(MyKeyboard);
+    glutMouseFunc(MousePressed);
+    glutMainLoop(); //  enter GLUT event processing loop
+    return 0;
+}
